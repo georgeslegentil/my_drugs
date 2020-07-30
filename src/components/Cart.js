b6555291ccc1492eecc47e2c7f7c4ead67232ea1
Georges <georges.legentil@epitech.eu>
2020-07-30 19:52:31 +0200

    test

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Cart extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Your cart is empty</h3>
+            </div>
+        )
+    }
+}
